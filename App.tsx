3b238268fb366e457cb4863bd2923097f5650e1c
walacefex <walacefex01@gmail.com>
2023-10-08 01:58:54 -0300

    remove import theme path mapping

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import { Groups } from "@screens/Groups";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/theme";
 
 export default function App() {
   return (
-  <Groups/>
+    <ThemeProvider theme={theme}>
+      <Groups/>
+    </ThemeProvider>
   );
 }
