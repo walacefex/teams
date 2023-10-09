2ba787ae6b40bd57b306c33afc163ef2a62c7491
walacefex <walacefex01@gmail.com>
2023-10-09 14:17:01 -0300

    Using custom fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { Groups } from "@screens/Groups";
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
   return (
     <ThemeProvider theme={theme}>
-      <Groups/>
+      {fontsLoaded ? <Groups/> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
