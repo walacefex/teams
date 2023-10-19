cadbe30c9f90a148829db74a018b1502564d74b6
walacefex <walacefex01@gmail.com>
2023-10-19 13:19:08 -0300

    Adding Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logoImg from "@assets/logo.png";
+
+import { BackButton, BackIcon, Container, Logo } from "./styles";
+
+type Props = {
+  showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: Props) {
+  return (
+    <Container>
+      { showBackButton &&
+        <BackButton>
+       <BackIcon />
+      </BackButton>}
+      <Logo source={ logoImg }/>
+    </Container>
+  );
+}
