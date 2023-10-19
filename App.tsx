0f266351a644d5654acbd1c28e441fd1a81ec9f7
walacefex <walacefex01@gmail.com>
2023-10-19 12:49:54 -0300

    adding styles on status bar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { Loading } from '@components/Loading';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { Groups } from "@screens/Groups";
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
 
@@ -12,6 +13,11 @@ export default function App() {
   });
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent" 
+        translucent 
+      />
       {fontsLoaded ? <Groups/> : <Loading />}
     </ThemeProvider>
   );
