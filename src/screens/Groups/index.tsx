1c6e5f0f3d3ed1f0904ad9270afc476468ddb831
walacefex <walacefex01@gmail.com>
2023-10-19 18:24:48 -0300

    Listing Group Card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,28 @@
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
      <Header />
      <Highlight
       title="Teams"
       subtitle='Play with your class'
+     />
+     <FlatList
+      data={groups}
+      keyExtractor={item => item}
+      renderItem={({ item }) => (
+        <GroupCard
+         title={item}
+        />
+      )}
      />
       <GroupCard title="Team 1" />
     </Container>
