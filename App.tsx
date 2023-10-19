5a2f5a89ec858910ad1972423866ca22f890fa79
walacefex <walacefex01@gmail.com>
2023-10-19 12:46:21 -0300

    adding loading because the fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
+import { Loading } from '@components/Loading';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { Groups } from "@screens/Groups";
-import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -11,7 +12,7 @@ export default function App() {
   });
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups/> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups/> : <Loading />}
     </ThemeProvider>
   );
 }
