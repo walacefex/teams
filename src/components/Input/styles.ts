9696ded14c97b13103a33f45a6b8b9df447abd08
walacefex <walacefex01@gmail.com>
2023-11-01 19:56:28 -0300

    Creating Input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,12 @@
+import { TextInput } from "react-native";
+import styled from "styled-components/native";
+
+export const Container = styled(TextInput)`
+  flex: 1;
+  min-height: 56px;
+  max-height: 56px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  border-radius: 6px;
+  padding: 16px;
+`;
