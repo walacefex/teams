dd6081bae3eaeab123b7c204597c1c2bb26c74d6
walacefex <walacefex01@gmail.com>
2023-11-01 19:59:56 -0300

    adding more props to font input

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -5,8 +5,11 @@ export const Container = styled(TextInput)`
   flex: 1;
   min-height: 56px;
   max-height: 56px;
+  border-radius: 6px;
   background-color: ${({ theme }) => theme.COLORS.GRAY_700};
   color: ${({ theme }) => theme.COLORS.WHITE};
-  border-radius: 6px;
+
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
   padding: 16px;
 `;
