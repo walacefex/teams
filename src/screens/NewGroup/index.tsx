5852399fbb2171e861453b7cbe97e0fb29669618
walacefex <walacefex01@gmail.com>
2023-11-01 19:51:05 -0300

    Creating new screen to add groups

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,21 @@
+import { Container, Content, Icon } from "./styles";
+
+import { Button } from "@components/Button";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+
+export function NewGroup(){
+  return (
+    <Container>
+     <Header showBackButton />
+     <Content>
+        <Icon />
+        <Highlight 
+          title="New Team" 
+          subtitle="crie a turma para adicionar as pessoas"
+        />
+        <Button title="Criar turma" />
+     </Content>
+    </Container>
+  )
+}
