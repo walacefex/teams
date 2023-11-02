72e77e095752fa4184b47e2200cc3d5d32aec511
walacefex <walacefex01@gmail.com>
2023-11-02 19:25:24 -0300

    Creating typing to routes and add function to redict in the button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,23 @@
+import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { Button } from '@components/Button';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
-import { useState } from 'react';
-import { FlatList } from 'react-native';
 
 import { ListEmpty } from '@components/ListEmpty';
 import { Container } from './styles';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('new',)
+  }
+
   return (
     <Container>
      <Header />
@@ -27,6 +36,10 @@ export function Groups() {
       contentContainerStyle={groups.length === 0 && { flex: 1 }}
       ListEmptyComponent={() => <ListEmpty message="No groups found" />}
       showsVerticalScrollIndicator={false}
+     />
+     <Button
+      title="Create group"
+      onPress={handleNewGroup}
      />
     </Container>
   );
