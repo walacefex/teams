2ce769adf1d9f402e543f60608a4da8e8669331c
walacefex <walacefex01@gmail.com>
2023-11-01 22:52:59 -0300

    fixed import of themeprovider

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { Loading } from '@components/Loading';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { Groups } from "@screens/Groups";
 import { StatusBar } from 'react-native';
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme";
 
 
