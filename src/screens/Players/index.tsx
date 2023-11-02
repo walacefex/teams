233a634fd167e2c01fd4cb9c71d1f7c37abaa71a
walacefex <walacefex01@gmail.com>
2023-11-01 22:51:20 -0300

    putting a message to list empty, removed the sidebar and adding button

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import { FlatList } from "react-native";
 import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
+import { Button } from '@components/Button';
 import { ButtonIcon } from "@components/Button/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { ListEmpty } from '@components/ListEmpty';
 import { PlayerCard } from '@components/PlayerCard';
 
 export function Players() {
@@ -53,6 +55,18 @@ export function Players() {
             onRemove={() => { }}
           />
         )}
+        ListEmptyComponent={() =>( 
+          <ListEmpty message="Não há pessoas nesse time." />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 }, 
+          players.length === 0 && { flex: 1 }
+        ]}
+      />
+      <Button 
+        title='Remover turma'
+        type='SECONDARY'
       />
     </Container>
   )
