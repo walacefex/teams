80c51c9e05d5cbf2b6c08e8aef1254bd34138898
walacefex <walacefex01@gmail.com>
2023-11-01 22:26:26 -0300

    Creating PlayerCard to listing all players

---
+++ src/components/PlayerCard/styles.ts
@@ -0,0 +1,30 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import styled, { css } from "styled-components/native";
+
+
+export const Container = styled.View`
+  width: 100%;
+  height: 56px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 16px;
+`;
+
+export const Name = styled.Text`
+  flex: 1;
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_200};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.MD}px;
+  `}
+`;
+
+
+export const Icon = styled(MaterialIcons).attrs(({ theme }) => ({
+  size: 24,
+  color: theme.COLORS.GRAY_200,
+}))`
+  margin-left: 16px;
+  margin-right: 4px;
+`;
