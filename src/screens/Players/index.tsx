2d263a197fd9e6a506064ee805775142eec0fab9
walacefex <walacefex01@gmail.com>
2023-11-01 22:17:49 -0300

    Using flatlist to render component Filter and creating views to group all

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,12 +1,16 @@
-import { Filter } from "@components/Filter";
-import { Container, Form } from "./styles";
+import { useState } from 'react';
+import { FlatList } from "react-native";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
 import { ButtonIcon } from "@components/Button/ButtonIcon";
+import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([]);
   return (
     <Container>
       <Header showBackButton />
@@ -22,8 +26,23 @@ export function Players() {
 
         <ButtonIcon icon="house" />
       </Form>
-      <Filter title="Time A" isActive />
-     
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
     </Container>
   )
 }
