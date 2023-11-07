bf9bc48f863055932b1615c9f4a3073874adbaf0
walacefex <walacefex01@gmail.com>
2023-11-06 21:21:29 -0300

    fix: chaning background color playercard component

--- src/components/PlayerCard/styles.ts
+++ src/components/PlayerCard/styles.ts
@@ -5,7 +5,7 @@ import styled, { css } from "styled-components/native";
 export const Container = styled.View`
   width: 100%;
   height: 56px;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  background-color: ${({ theme }) => theme.COLORS.GRAY_500};
   border-radius: 6px;
   flex-direction: row;
   align-items: center;
